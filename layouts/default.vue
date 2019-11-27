26cc244de667b9883c19844eb231971da45bc9f7
caoqiang <caoqiang@csii.com.cn>
2019-11-27 18:03:32 +0800

    初始化

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      <router-link to="/">首页</router-link>
+      <router-link to="/inspire">活动</router-link>
+    </header>
+    <!-- 内容区域 pages文件夹下面的内容 -->
+    <nuxt></nuxt>
+    <footer>页面底部</footer>
+  </div>
+</template>
---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="item in hotCity" :v-for="item.id">
+    {{item.name}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  asyncData () {
+    return axios.get('http://47.106.189.145/api/city.json')
+    .then(res => {
+      return {
+        hotCity: res.data.data.hotCity
+      }
+    })
+  }
+}
+</script>
